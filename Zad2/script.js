cf3a04b086fed2ea6c18de797d32250ab27bfa68
AleksandraYevt <yevtushenko.aleksa@gmail.com>
2023-05-19 14:53:49 +0200

    zad2 i zad3

--- Zad2/script.js
+++ Zad2/script.js
@@ -1,8 +1,14 @@
 let currentColor = null;
 
+const config = {
+  redTime: 3000,
+  yellowTime: 1000,
+  greenTime: 1000
+};
+
 function changeColor(color) {
   const trafficLights = document.getElementsByClassName("traffic-light");
-  
+
   if (currentColor !== null) {
     document.getElementById(currentColor).style.backgroundColor = "gray";
   }
@@ -11,30 +17,34 @@ function changeColor(color) {
   currentColor = color;
 }
 
-setInterval(changeTrafficLight, 5000);
-
 function changeTrafficLight() {
-  const trafficLights = document.getElementsByClassName("traffic-light");
+  const trafficLights = document.getElementsByClassName("traffic_light");
 
   switch (currentColor) {
     case "red":
       document.getElementById("red").style.backgroundColor = "gray";
       document.getElementById("yellow").style.backgroundColor = "yellow";
       currentColor = "yellow";
+      setTimeout(changeTrafficLight, config.yellowTime);
       break;
     case "yellow":
       document.getElementById("yellow").style.backgroundColor = "gray";
       document.getElementById("green").style.backgroundColor = "green";
       currentColor = "green";
+      setTimeout(changeTrafficLight, config.greenTime);
       break;
     case "green":
       document.getElementById("green").style.backgroundColor = "gray";
       document.getElementById("red").style.backgroundColor = "red";
       currentColor = "red";
+      setTimeout(changeTrafficLight, config.redTime);
       break;
     default:
       document.getElementById("red").style.backgroundColor = "red";
       currentColor = "red";
+      setTimeout(changeTrafficLight, config.redTime);
       break;
   }
 }
+
+setTimeout(changeTrafficLight, config.redTime);
